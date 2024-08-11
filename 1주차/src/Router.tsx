e617f852cb5d7bc6a39350bad21455b41e98e876
박지연 <163213733+j02on@users.noreply.github.com>
2024-08-11 15:07:52 +0900

    NoticeModificationPage 라우터 설정

--- 1주차/src/Router.tsx
+++ 1주차/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { SignUpPage } from "./pages/SignUpPage";
 import { LoginPage } from "./pages/LoginPage";
 import { PostListInquiryPage } from "./pages/PostListInquiryPage";
+import { NoticeModificationPage } from "./pages/NoticeModificationPage";
 
 export const AppRouter = () => {
   return (
@@ -10,6 +11,10 @@ export const AppRouter = () => {
         <Route path="/SignUp" element={<SignUpPage />} />
         <Route path="Login" element={<LoginPage />} />
         <Route path="/PostListInquiry" element={<PostListInquiryPage />} />
+        <Route
+          path="/NoticeModification"
+          element={<NoticeModificationPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
