b66863b52407f1d4ca97e225245ccf97bca56217
박지연 <163213733+j02on@users.noreply.github.com>
2024-08-11 15:42:10 +0900

    PostList 수정

---
+++ 1주차/src/components/PostListInquiryPage/PostList.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import fonts from "../../design-system/fonts/fonts";
+import React from "react";
+
+export const PostList = () => {
+  return (
+    <PostListContainer>
+      <PostListContents>
+        <PostListTitle>제목이 들어가요</PostListTitle>
+        <PostListDate>22/08/16</PostListDate>
+      </PostListContents>
+    </PostListContainer>
+  );
+};
+
+const PostListContents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const PostListContainer = styled.div`
+  width: 1120px;
+  padding: 20px 40px;
+  /* height: 70px; */
+  border-radius: 4px;
+  box-shadow: 0 1px 15px 0 rgb(0, 0, 0, 0.1);
+`;
+
+const PostListTitle = styled.div`
+  font-family: NotoSansKR;
+  font-size: 22px;
+  font-weight: 400;
+`;
+
+const PostListDate = styled.div`
+  font: ${fonts.Body1};
+`;
